1b66833fe56362517da8d11d724463294d5468e2
Paulo Cesar <paulocesarmedeiross@gmail.com>
2024-10-11 17:38:38 -0300

    estado de botoes

--- sketch.js
+++ sketch.js
@@ -28,6 +28,16 @@ let handPose;
 let video;
 let hands = [];
 
+let handThumb, handIndex;
+
+//? Animação ao fazer movimento de pinça
+let clickStartTime;
+let clickDuration = 2000;
+let clickRadius = 10;
+
+let gameState = 'main-menu';
+let startButton, instructionsButton;
+
 // Under the Sea, I Will Survive, Shape of You, Kulikitaka, Chacarron
 
 function preload(){
@@ -39,21 +49,18 @@ function setup() {
 
 	frameRate(60);
 	
-	video = createCapture(VIDEO);
+	//? ===== CAMERA =====
+	video = createCapture(video);
 	video.size(960, 540);
 	video.hide();
 	// start detecting hands from the webcam video
 	handPose.detectStart(video, gotHands);
+	//? ===== CAMERA =====
 
 	handController =  new Sprite();
 		handController.width = 20;
 		handController.height = 20;
-
-	// timerController = new Sprite();
-	// 	timerController.width = 150;
-	// 	timerController.height = 75
-	// 	timerController.y = width-timerController.h/2
-	// 	timerController.collider = 'none'
+		handController.visible = false
 
 	leftTargets = new Group();
 		leftTargets.color = 'yellow';
@@ -68,96 +75,126 @@ function setup() {
 		rightTargets.diameter = 10;
 		rightTargets.collider = 'none'
 
-
-	// moveSpawner = new Sprite(width, height-25, 50, 50, 'none');
-	// 	//moveSpawner.vel.x = -3
-	// 	moveSpawner.sequence = [0, 1, 1, 1, 3, 3, 2, 2];
-
 	moveSpawnerGroup = new Group();
-		//moveSpawnerGroup.vel.x = -2;
 		moveSpawnerGroup.sequence = spawnerController;
 
 	danceMoveController = new Sprite(width/5, height-50, 10, 100, 'static');
+		danceMoveController.visible = false;
 	textSize(20)
-	// while(leftTargets.length < 10){
-	// 	let target = new leftTargets.Sprite();
-	// 	target.x = leftTargets.length * 25;
-	// }
 
-	
+	handIndex = new Sprite();
+		handIndex.w = handIndex.h = 25;
+	handThumb = new Sprite();
+		handThumb.w = handThumb.h = 25;
+		handThumb.collider = handIndex.collider = 'none'
+
+	startButton = new Sprite(width/2, height/2, 250, 75, 'none');
+		startButton.layer = -1;
+	instructionsButton = new Sprite();
+		instructionsButton.w = 250;
+		instructionsButton.h = 75;
+		instructionsButton.y = 350;
+		instructionsButton.collider = 'none'
+		instructionsButton.layer = -1
+
+	clickStartTime = millis();
 }
 
 function draw() {
 	background('skyblue');
 
+	//? ===== CAMERA =====
 	push();
 		translate(video.width, 0);
 		scale(-1, 1);
 		image(video, -1, 1, width, height);
     pop();
+
 	
+	//? ===== CAMERA =====
 
-	// Draw all the tracked hand points
-	for (let i = 0; i < hands.length; i++) {
-		let hand = hands[i];
-		for (let j = 0; j < hand.keypoints.length; j++) {
-		let keypoint = hand.keypoints[j];
-		fill(0, 255, 0);
-		noStroke();
-		circle(width - keypoint.x, keypoint.y, 10);
-		}
+	push()
+		if(handIndex.overlapping(handThumb)){
+			if(handIndex.overlapping(startButton) && handThumb.overlapping(startButton)){
+				clickAnimation() > 360 ? gameState = 'game' : null;
+			} else if(handIndex.overlapping(instructionsButton) && handThumb.overlapping(instructionsButton)){
+				clickAnimation() > 360 ? gameState = 'instructions' : null;
+			}
+
+		} else clickStartTime = millis();
+	pop()
+
+	if(gameState == 'main-menu'){
+		startButton.x = width/2
+		startButton.y = height/2
 	}
+	else if(gameState == 'instructions'){
+		instructionsButton.remove();
+		startButton.x = width - 140;
+		startButton.y = height - 75;
+	} 
+	else if(gameState == 'game'){
+		timeCount = round(frameCount/60, 2);
+		text(timeCount, 100, 100);
 
-	timeCount = round(frameCount/60, 2);
-	text(timeCount, 100, 100);
+		//* Controle do usuário
+		//^		Fazer controle utilizando as mãos
+		//^		Fazer movimento de pinça para seleção
+		handController.x = mouse.x
+		handController.y = mouse.y
 
-	//* Controle do usuário
-	//^		Fazer controle utilizando as mãos
-	//^		Fazer movimento de pinça para seleção
-	handController.x = mouse.x
-	handController.y = mouse.y
-
-	if(moveSpawnerGroup.length <= 0 && isGameStarted == false){
-		isGameStarted = true
-		spawnerController.forEach((item) => {
-			let spawnerSprite = new moveSpawnerGroup.Sprite();
-			spawnerSprite.x = width + 25
-			spawnerSprite.y = height - 50
-			spawnerSprite.collider = 'none'
-			spawnerSprite.sequence = {time: item.time, move: item.move}
-		})
-	}
-
-	if(moveSpawnerGroup.length <= 0) text('Jogo Completo', 150, 150);
-
-	if(moveSpawnerGroup.length > 0){
-		if(moveSpawnerGroup[0].overlaps(danceMoveController)){
-			moveSpawnerGroup[0].life = 60*moveSpawnerGroup[0].sequence.time
-			leftTargets.removeAll()
-			rightTargets.removeAll();
-			let getMovement = moveSpawnerGroup[0].sequence.move
-			setTimeout(() => { 
-				generatePath(leftTargets, getMovement) 
-				generatePath(rightTargets, 3)
-				console.log('teste');
-			}, 250)
-			leftTargets.life = 60*moveSpawnerGroup[0].sequence.time
-			rightTargets.life = 60*moveSpawnerGroup[0].sequence.time
+		if(moveSpawnerGroup.length <= 0 && isGameStarted == false){
+			isGameStarted = true
+			spawnerController.forEach((item) => {
+				let spawnerSprite = new moveSpawnerGroup.Sprite();
+				spawnerSprite.x = width + 25
+				spawnerSprite.y = height - 50
+				spawnerSprite.collider = 'none'
+				spawnerSprite.sequence = {time: item.time, move: item.move}
+			})
 		}
-		if(moveSpawnerGroup[0].overlapping(danceMoveController)){
-			moveSpawnerGroup[0].vel.x = 0
-			// if(handController.overlaps(leftTargets[0])){
-			// 	leftTargets[0].remove();
-			// }
-		} else {
-			moveSpawnerGroup[0].vel.x = -5;
-		}
-		//(moveSpawnerGroup[0].x < - 20) ? moveSpawnerGroup.shift() : null;
-		if(leftTargets.length > 0 && moveSpawnerGroup[0].overlapping(danceMoveController)){
-			if(handController.overlaps(leftTargets[0])){
-				leftTargets[0].remove();
+
+		if(moveSpawnerGroup.length <= 0) text('Jogo Completo', 150, 150);
+
+		if(moveSpawnerGroup.length > 0){
+			if(moveSpawnerGroup[0].overlaps(danceMoveController)){
+				moveSpawnerGroup[0].life = 60*moveSpawnerGroup[0].sequence.time
+				leftTargets.removeAll()
+				rightTargets.removeAll();
+				let getMovement = moveSpawnerGroup[0].sequence.move
+				setTimeout(() => { 
+					generatePath(leftTargets, getMovement) 
+					generatePath(rightTargets, 3)
+					//console.log('teste');
+				}, 250)
+				leftTargets.life = 60*moveSpawnerGroup[0].sequence.time
+				rightTargets.life = 60*moveSpawnerGroup[0].sequence.time
+			}
+			if(moveSpawnerGroup[0].overlapping(danceMoveController)){
+				moveSpawnerGroup[0].vel.x = 0
+				// if(handController.overlaps(leftTargets[0])){
+				// 	leftTargets[0].remove();
+				// }
+			} else {
+				moveSpawnerGroup[0].vel.x = -5;
+			}
+			//(moveSpawnerGroup[0].x < - 20) ? moveSpawnerGroup.shift() : null;
+			if(leftTargets.length > 0 && moveSpawnerGroup[0].overlapping(danceMoveController)){
+				if(handController.overlaps(leftTargets[0])){
+					leftTargets[0].remove();
+				}
 			}
 		}
+
+	}
+
+	if(hands.length > 0){
+		//console.log(hands)
+		let { thumb_tip, index_finger_tip } = hands[0];
+		handThumb.x = width - thumb_tip.x;
+		handThumb.y = thumb_tip.y;
+		handIndex.x = width - index_finger_tip.x;
+		handIndex.y = index_finger_tip.y;
 	}
 }
 
@@ -236,5 +273,24 @@ function up(leftTargets){
 function gotHands(results) {
 	// save the output to the hands variable
 	hands = results;
+}
+
+function clickAnimation(){
+	let elapsedTime = millis() - clickStartTime;
+  
+	// Calcula a fração do tempo até o máximo de 2 segundos
+	//let progress = min(elapsedTime / clickDuration, 1);
+	let progress = handIndex.overlapping(handThumb);
+	
+	// Converte o progresso em um ângulo
+	let endAngle = progress * PI;
+	
+	// Desenha a circunferência parcialmente
+	fill('white');
+	stroke(0);
+	strokeWeight(5);
+	arc(30, 30, 25 * 2, 25 * 2, 0, endAngle);
+	
+	return endAngle;
 }
 
